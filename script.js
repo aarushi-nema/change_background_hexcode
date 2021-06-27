b57049cc14e655bcb99dba0c0a7a3b42fa0d3466
c4t-ajay <ajay@cloud4things.com>
2021-06-27 13:17:25 +0530

    reupload

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const button= document.querySelector('.color_changer_button');
 const body= document.querySelector('body');
 const hexvalues= [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
 const hexcode= document.querySelector('.hex_container');
+const defaultColor= '#0000ff'; 
 
+hexcode.textContent= defaultColor;
+body.style.backgroundColor= defaultColor;
 
 
 button.addEventListener('click', changeColor);
